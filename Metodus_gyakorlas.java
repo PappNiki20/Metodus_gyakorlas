8bbc19dcbb5f94d9ccf8d5f8e05ec09f4cf20448
PappNiki20 <115682617+PappNiki20@users.noreply.github.com>
2023-02-22 12:40:02 +0100

    negy szam osszege

--- Metodus_gyakorlas.java
+++ Metodus_gyakorlas.java
@@ -1,14 +1,18 @@
 
 package metodus_gyakorlas;
 
+
+
 public class Metodus_gyakorlas {
 
     
     public static void main(String[] args) {
        
-        
-        kiir("Az első 10 szám összege: "+elso10szamosszege());
-        kiir("\nA a + b = "+osszead(2,3));
+       int osszeg = osszead(3,6);
+       int a = 3, b= 6;
+      kiir("Az első 10 szám összege: "+elso10szamosszege());
+     // kiir("\n%d + %d = %d".formatted(a,b,osszeg));
+      kiir("\n4 szám összege: "+negyszamosszeg(2,3,4,5));
     }
 
     private static int elso10szamosszege() {
@@ -18,14 +22,25 @@ public class Metodus_gyakorlas {
         }
         return ossz;
     }
-
+    
     private static int osszead(int a, int b) {
        int osszeaderedmeny = a+b;
         return osszeaderedmeny;
     }
 
     private static void kiir(String szoveg) {
-        System.out.printf(szoveg);
+        System.out.print(szoveg);
+    }
+
+    private static int negyszamosszeg(int szam1,int szam2,int szam3,int szam4) {
+        int ossz = 0;
+        
+        int[] szamok = {szam1,szam2,szam3,szam4};
+        for (int i = 0; i < szamok.length; i++) {
+           ossz += szamok[i];
+            
+        }
+        return ossz;
     }
    
     

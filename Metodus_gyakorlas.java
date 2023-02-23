e196c9d5e67b30e0b27cf98310b7ea2c0682e8aa
PappNiki20 <115682617+PappNiki20@users.noreply.github.com>
2023-02-23 12:24:09 +0100

    Tegnapi orai munka

--- Metodus_gyakorlas.java
+++ Metodus_gyakorlas.java
@@ -8,11 +8,12 @@ public class Metodus_gyakorlas {
     
     public static void main(String[] args) {
        
-       int osszeg = osszead(3,6);
-       int a = 3, b= 6;
+       int osszeg = osszead(1,2);
+       int gyokosszeg =osszead(3,4); 
       kiir("Az első 10 szám összege: "+elso10szamosszege());
-     // kiir("\n%d + %d = %d".formatted(a,b,osszeg));
-      kiir("\n4 szám összege: "+negyszamosszeg(2,3,4,5));
+      kiir("\nNégy szám összege = %d".formatted(osszeg));
+      kiir("\n gyöke: "+osszeggyok(gyokosszeg,4));
+//      kiir("\n4 szám összege: "+negyszamosszeg(2,3,4,5));
     }
 
     private static int elso10szamosszege() {
@@ -25,6 +26,8 @@ public class Metodus_gyakorlas {
     
     private static int osszead(int a, int b) {
        int osszeaderedmeny = a+b;
+        osszeaderedmeny =osszeaderedmeny+3;
+        osszeaderedmeny = osszeaderedmeny+4;
         return osszeaderedmeny;
     }
 
@@ -32,15 +35,20 @@ public class Metodus_gyakorlas {
         System.out.print(szoveg);
     }
 
-    private static int negyszamosszeg(int szam1,int szam2,int szam3,int szam4) {
-        int ossz = 0;
-        
-        int[] szamok = {szam1,szam2,szam3,szam4};
-        for (int i = 0; i < szamok.length; i++) {
-           ossz += szamok[i];
-            
-        }
-        return ossz;
+//    private static int negyszamosszeg(int szam1,int szam2,int szam3,int szam4) {
+//        int ossz = 0;
+//        
+//        int[] szamok = {szam1,szam2,szam3,szam4};
+//        for (int i = 0; i < szamok.length; i++) {
+//           ossz += szamok[i];
+//            
+//        }
+//        return ossz;
+//    }
+
+    private static double osszeggyok(int ab ,int c) {
+       double gyok = Math.sqrt(ab+c);
+       return gyok;
     }
    
     
---
+++ Szineskiiras.java
@@ -0,0 +1,26 @@
+
+package metodus_gyakorlas;
+
+
+
+
+public class Szineskiiras {
+    public static void main(String[] args) {
+        equalizer(2);
+        equalizer(4);
+        equalizer(1);
+        equalizer(4);
+        equalizer(5);
+    }
+
+    private static void equalizer(int db) {
+        for (int i = 0; i < db; i++) {
+         int szam = (int) (Math.random () * ((7 -3) +1) + 3);
+         String szin ="\u001B[45m";
+          for (int j = 0; j < szam; j++) {
+               System.out.print( szin + " ");
+        }
+          System.out.println("");
+    }
+    }
+}

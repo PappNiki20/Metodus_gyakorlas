b9dd45bc6eb39630b0fcf6098ee2c2bcecd8044b
PappNiki20 <115682617+PappNiki20@users.noreply.github.com>
2023-02-23 12:26:04 +0100

    javitva

--- Szineskiiras.java
+++ Szineskiiras.java
@@ -8,13 +8,23 @@ public class Szineskiiras {
     public static void main(String[] args) {
         equalizer(2);
         equalizer(4);
-        equalizer(1);
-        equalizer(4);
+        equalizer();
+        equalizer();
         equalizer(5);
     }
 
     private static void equalizer(int db) {
         for (int i = 0; i < db; i++) {
+         int szam = (int) (Math.random () * ((7 -3) +1) + 3);
+         String szin ="\u001B[45m";
+          for (int j = 0; j < szam; j++) {
+               System.out.print( szin + " ");
+        }
+          System.out.println("");
+    }
+    }
+     private static void equalizer() {
+        for (int i = 0; i < 5; i++) {
          int szam = (int) (Math.random () * ((7 -3) +1) + 3);
          String szin ="\u001B[45m";
           for (int j = 0; j < szam; j++) {
